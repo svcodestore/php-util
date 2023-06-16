53f3c243a0d9f7606939ebea077217823d4f1f0d
yangwb1123 <1832227346@qq.com>
2023-06-16 16:03:00 +0800

    feat(rpc): `oauth` util add `getMenus`,`logout`

--- src/Oauth.php
+++ src/Oauth.php
@@ -6,7 +6,7 @@ namespace Sv\Util;
 
 class Oauth
 {
-    public static function getCurrentApplication(int $appId, string $serverAddress): array
+    public static function getAppInfo(string $serverAddress, int $appId): array
     {
         $client = new \Proto_app\AppClient($serverAddress, [
             'credentials' => \Grpc\ChannelCredentials::createInsecure(),
@@ -17,9 +17,40 @@ class Oauth
 
         list($rpcResponse, $status) = $client->GetAppById($rpcRequest)->wait();
 
-        if ($status->code !== \Grpc\STATUS_OK) {
-            echo "ERROR: " . $status->code . ", " . $status->details . PHP_EOL;
-        }
+        $json = $rpcResponse->getResult()->serializeToJsonString();
+        $client->close();
+
+        return [$json, $status];
+    }
+
+    public static function getAppMenus(string $serverAddress, int $appId, int $userId): array
+    {
+        $client = new \Proto_app\AppClient($serverAddress, [
+            'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+        ]);
+
+        $rpcRequest = new \Proto_app\GetAhorizedMenusByAppIdAndUserIdRequest();
+        $rpcRequest->setAppId($appId);
+        $rpcRequest->setUserId($userId);
+
+        list($rpcResponse, $status) = $client->GetAhorizedMenusByAppIdAndUserId($rpcRequest)->wait();
+
+        $json = $rpcResponse->getResult()->serializeToJsonString();
+        $client->close();
+
+        return [$json, $status];
+    }
+
+    public static function logout(string $serverAddress, int $userId): array
+    {
+        $client = new \Proto_auth\AuthClient($serverAddress, [
+            'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+        ]);
+
+        $rpcRequest = new \Proto_auth\LogoutRequest();
+        $rpcRequest->setUserId($userId);
+
+        list($rpcResponse, $status) = $client->Logout($rpcRequest)->wait();
 
         $json = $rpcResponse->getResult()->serializeToJsonString();
         $client->close();
