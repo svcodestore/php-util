e2bae8cfef1e643ad44f45c3264586a27bb315b6
yangwb1123 <1832227346@qq.com>
2025-04-21 09:32:28 +0800

    fix: curl header content type json

--- src/Curl.php
+++ src/Curl.php
@@ -34,43 +34,45 @@ class Curl
 
     public static function post(string $url, $params = [], array $headers = [])
     {
-        $curl = curl_init();
+        try {
+            $curl = curl_init();
 
-        $opt = [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-        ];
+            $opt = [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+            ];
 
 
-        if (!empty($params)) {
-            if (!empty($headers)) {
-                $headers[] = ['Content-Type: application/x-www-form-urlencoded'];
-            } else {
-                $headers = ['Content-Type: application/x-www-form-urlencoded'];
+            if (!empty($params)) {
+                if (!empty($headers)) {
+                    $headers[] = ['Content-Type: application/x-www-form-urlencoded'];
+                } else {
+                    $headers = ['Content-Type: application/x-www-form-urlencoded'];
+                }
+                if (in_array('Content-Type: application/x-www-form-urlencoded', $headers)) {
+                    $opt[CURLOPT_POSTFIELDS] = http_build_query($params);
+                } elseif (in_array('Content-Type: application/json', $headers)) {
+                    $opt[CURLOPT_POSTFIELDS] = json_encode($params);
+                }
             }
-            if (in_array('Content-Type: application/x-www-form-urlencoded', $headers)) {
-                $opt[CURLOPT_POSTFIELDS] = http_build_query($params);
-            } elseif (in_array('Content-Type: application/json', $headers)) {
-                $opt[CURLOPT_POSTFIELDS] = $params;
-            }
+
+            curl_setopt_array(
+                $curl,
+                $opt
+            );
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            return $response;
+        } catch (\Throwable $th) {
+            return $th;
         }
-
-        $opt[CURLOPT_HTTPHEADER] = $headers;
-
-        curl_setopt_array(
-            $curl,
-            $opt
-        );
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        return $response;
     }
 }
